14eea70ed897fa0cdae00776dda651f835afd0a3
Santosh Baburao <santosh.baburao@gmail.com>
2018-06-12 17:38:11 +0530

    Delete erp_issues_item.js
    delete

--- rollon_erpnext/rollon_erpnext/doctype/erp_issues_item/erp_issues_item.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2018, MN Technique and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('ERP Issues Item', {
-	refresh: function(frm) {
-
-	}
-});
