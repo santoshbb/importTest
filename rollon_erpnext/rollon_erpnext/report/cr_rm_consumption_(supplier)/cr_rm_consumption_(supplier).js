cb47a1bfca37ffb7ec5747b5951860afcada93bf
santoshbb <santosh.baburao@gmail.com>
2018-11-05 12:50:15 +0530

    Item changes

---
+++ rollon_erpnext/rollon_erpnext/report/cr_rm_consumption_(supplier)/cr_rm_consumption_(supplier).js
@@ -0,0 +1,33 @@
+frappe.query_reports["CR RM Consumption (Supplier)"] = {
+"filters": [
+{
+"fieldname" : "from_date",
+"label":("From Date"),
+"fieldtype":"Date",
+"default":frappe.datetime.month_start(date),
+"reqd":1
+},
+
+{
+"fieldname":"to_date",
+"label":("To Date"),
+"fieldtype":"Date",
+"default":get_today(),
+"reqd":1
+},
+
+{
+"fieldname" : "item_group",
+"label":("Item Group"),
+"fieldtype":"Select",
+"options" : ["", "Raw Material"],
+"default":"Raw Material"
+}
+
+
+
+]
+
+}
+
+
